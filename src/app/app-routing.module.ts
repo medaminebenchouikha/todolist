99726e1d850ea58de1cdbda18bafb84d16e3ddd0
Med Amine Ben Chouikha <med.amine.ben.chouikha@gmail.com>
2019-10-23 11:17:24 +0200

    mise a jour todolist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TodoListComponent } from './components/user/todo-list/todo-list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { TodoAddComponent } from './components/user/todo-add/todo-add.component';
+import { TodoUpdateComponent } from './components/user/todo-update/todo-update.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //homeroute
+  {
+    path:'',
+    component:HomeComponent,
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+      path:'user/todo-list',
+      component:TodoListComponent
+    },
+    {
+      path:'user/todo-add',
+      component:TodoAddComponent
+    },
+    {
+      path:'user/todo-update',
+      component:TodoUpdateComponent
+    }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TodoListComponent } from './components/user/todo-list/todo-list.component';
+import { TodoAddComponent } from './components/user/todo-add/todo-add.component';
+import { TodoUpdateComponent } from './components/user/todo-update/todo-update.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    TodoListComponent,
+    TodoAddComponent,
+    TodoUpdateComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
